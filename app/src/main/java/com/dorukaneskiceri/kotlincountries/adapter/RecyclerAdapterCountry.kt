fe595598485bc6b96cd197946b96724481d9bc77
DorukanE <dorukaneskiceri@gmail.com>
2021-02-02 17:40:23 +0300

    feat: Recycler Adapter created

---
+++ app/src/main/java/com/dorukaneskiceri/kotlincountries/adapter/RecyclerAdapterCountry.kt
@@ -0,0 +1,38 @@
+package com.dorukaneskiceri.kotlincountries.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.dorukaneskiceri.kotlincountries.R
+import com.dorukaneskiceri.kotlincountries.model.Country
+import kotlinx.android.synthetic.main.fragment_country.view.*
+import kotlinx.android.synthetic.main.recycler_view_countries.view.*
+
+class RecyclerAdapterCountry(private val arrayListCountries: ArrayList<Country>) : RecyclerView.Adapter<RecyclerAdapterCountry.CountryHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CountryHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.recycler_view_countries, parent, false)
+        return CountryHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: CountryHolder, position: Int) {
+        holder.view.textViewCountry.text = arrayListCountries[position].countryName
+        holder.view.textViewCountryContinental.text = arrayListCountries[position].countryContinental
+    }
+
+    override fun getItemCount(): Int {
+        return arrayListCountries.size
+    }
+
+    class CountryHolder(var view: View): RecyclerView.ViewHolder(view){
+
+    }
+
+    fun updateCountryList(newCountryList: List<Country>){
+        arrayListCountries.clear()
+        arrayListCountries.addAll(newCountryList)
+        notifyDataSetChanged() 
+    }
+}
---
+++ app/src/main/java/com/dorukaneskiceri/kotlincountries/model/Model.kt
@@ -0,0 +1,9 @@
+package com.dorukaneskiceri.kotlincountries.model
+
+data class Country(
+    val countryName: String?,
+    val countryCapital: String?,
+    val countryContinental: String?,
+    val countryCurrency: String?,
+    val countryFlagUrl: String?,
+    val countryLanguage: String?)
